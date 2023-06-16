8d8b1b375c55d8cf16ee633c69ba607adca9e9ef
AndriiKulachenko <kulachenkoa@UKR.NET>
2023-06-16 17:42:22 +0300

    dasd

--- index.js
+++ index.js
@@ -1,7 +1,40 @@
-document.querySelector('.burger').addEventListener('click', function(){
+const burger = document.querySelector('.burger');// burger button
+const menuContainer = document.querySelector('.menu-container');
+const overlayDiv = document.querySelector('.mobile-menu-overlay'); // overlay div
+
+burger.addEventListener('click', function(){
     this.classList.toggle('active');
-    document.querySelector('.menu-container').classList.toggle('open');
+    menuContainer.classList.toggle('open');
+    overlayDiv.classList.toggle('on');
+});
+
+overlayDiv.addEventListener('click', function(){
+     overlayDiv.classList.remove('on');
+     menuContainer.classList.remove('open');
+     burger.classList.remove('active');   
+ });
+
+
+const Features = document.getElementById("features");   // Spusok Features
+const SubNav = document.querySelector('.sub-nav');
+
+
+
+Features.addEventListener('click', function(){
+    if (window.innerWidth < 769) {
+        Features.classList.toggle('list-features');
+        SubNav.classList.toggle('list-features-nav');
+    }
+    
+});
+
+const company = document.getElementById("company"); // Spusok Company
+const companyList = document.getElementById("companyList");
+
+company.addEventListener('click',function(){
+    if (window.innerWidth < 769) {
+        company.classList.toggle('list-features');
+        companyList.classList.toggle('list-features-nav');
+    }
+    
 })
-
-
-
